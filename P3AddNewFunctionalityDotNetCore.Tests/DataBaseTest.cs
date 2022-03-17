7e69f066ee4f41d64f70cf692afba456daa45361
clems67 <caravita.clement.67150@gmail.com>
2022-03-17 11:13:52 +0100

    create files DataBaseTest & ProductControllerTest

---
+++ P3AddNewFunctionalityDotNetCore.Tests/DataBaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using P3AddNewFunctionalityDotNetCore.Data;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public abstract class DataBaseTest
+    {
+        public DbContextOptions<P3Referential> builder;
+        public P3Referential context;
+        public DataBaseTest()
+        {
+            builder = new DbContextOptionsBuilder<P3Referential>().UseSqlServer($"Server=.\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+            context = new P3Referential(builder);
+
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
---
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Controllers;
+using P3AddNewFunctionalityDotNetCore.Data;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class ProductControllerTest : DataBaseTest
+    {
+        public readonly IProductService productService;
+        public readonly ProductService productService2;
+        public readonly ILanguageService languageService;
+        public readonly IProductRepository productRepository;
+        public readonly ICart cart;
+        public readonly IOrderRepository orderRepository;
+        public readonly StringLocalizer<ProductService> localizer;
+        public ProductViewModel product;
+        public readonly ProductController productController;
+        public readonly P3Referential p3Referential;
+        public ProductControllerTest()
+        {
+            p3Referential = new P3Referential(builder);
+
+            productRepository = new ProductRepository(p3Referential);
+            productService = new ProductService(cart, new ProductRepository(p3Referential), orderRepository, localizer);
+
+            productController = new ProductController(productService, languageService);
+            product = new ProductViewModel();
+        }
+    }
+}
