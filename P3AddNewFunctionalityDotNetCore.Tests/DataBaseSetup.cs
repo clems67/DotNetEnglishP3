d377d75023fe44bb7d8932719e6a11b1ca78e858
clems67 <caravita.clement.67150@gmail.com>
2022-04-08 00:13:04 +0200

    setting up a database for the test
    create a class to be used multiple times in my integration tests

---
+++ P3AddNewFunctionalityDotNetCore.Tests/DataBaseSetup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Data;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class DataBaseSetup
+    {
+        public IServiceCollection services;
+        public ServiceProvider serviceProvider;
+        public DataBaseSetup()
+        {
+            Mock<IConfigurationSection> configurationSectionStub = new Mock<IConfigurationSection>();
+            configurationSectionStub.Setup(x => x["DefaultConnection"]).Returns("TestConnectionString");
+            Mock<Microsoft.Extensions.Configuration.IConfiguration> configurationStub = new Mock<Microsoft.Extensions.Configuration.IConfiguration>();
+            configurationStub.Setup(x => x.GetSection("ConnectionStrings")).Returns(configurationSectionStub.Object);
+            configurationSectionStub.Setup(x => x["P3Referential"])
+                .Returns("Server=.\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //configurationStub.Setup(x => x.GetConnectionString("P3Referential")).Returns("Server=.\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true");
+            services = new ServiceCollection();
+            var target = new Startup(configurationStub.Object);
+            serviceProvider = services.BuildServiceProvider();
+            target.ConfigureServices(services);
+        }
+    }
+}
