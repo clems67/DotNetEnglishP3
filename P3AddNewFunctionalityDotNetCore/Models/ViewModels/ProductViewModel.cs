65f13ea9e0c7f20730446e6166faaae6b494e434
clems67 <caravita.clement.67150@gmail.com>
2022-06-16 16:10:40 +0200

    fixing the regular expressions

--- P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -7,18 +7,19 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
     {
         [BindNever]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
-        [Required]
-        [Range(1, int.MaxValue)]
-        [RegularExpression(@"^([1-9][0-9]*)$", ErrorMessage = "stocksth")]
+        [Required(ErrorMessage = "MissingQuantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "StockNotAnInteger")]
+        //[RegularExpression(@"^([1-9][0-9]*)$", ErrorMessage = "StockNotAnInteger")]
         public string Stock { get; set; }
         [Required(ErrorMessage = "MissingPrice")]
-        [RegularExpression(@"^([0-9]+[.|,]?[0-9]*)$", ErrorMessage = "PriceNotANumber")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "PriceNotANumber")]
+        //[Range(1, float.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
         public string Price { get; set; }
     }
 }
